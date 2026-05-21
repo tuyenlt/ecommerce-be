2ae24a6cb4f83fc37a24baae3d5e68b6d8c9c0a9
tuyenlt <keplerlu@gmail.com>
2026-05-21 23:22:15 +0700

    fix: cors env load

--- src/main.ts
+++ src/main.ts
@@ -61,6 +61,7 @@ async function bootstrap() {
       process.env.ADMIN_URL?.trim(),
       "http://localhost:5173",
       "http://localhost:5174",
+	  "https://admin.ecommerce.tmsherk.id.vn",
     ],
     // alway enable CORS for all origins
     // origin: "*",
